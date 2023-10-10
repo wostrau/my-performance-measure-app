bad05e19c49ca107870f7dbd93b403fe2de777ee
wostrau <a.ostrovtsov@gmail.com>
2023-10-10 14:17:48 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# my-performance-measure-app
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const CleanPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    shop: './src/non-optimized/shop.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist', 'assets', 'scripts'),
+    publicPath: 'assets/scripts/',
+  },
+  devtool: 'eval-cheap-module-source-map',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    client: {
+      overlay: true, // This displays build errors on the webpage
+    },
+    devMiddleware: {
+      publicPath: '/assets/scripts/', // Adjust this path as needed
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                { useBuiltIns: 'usage', corejs: { version: 3 } }
+              ]
+            ]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [new CleanPlugin.CleanWebpackPlugin()]
+};
